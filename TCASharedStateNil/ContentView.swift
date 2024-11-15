cfca833384b0cc882728d1e4ebe67b98073de3dd
Kristiyan Dobrev <kdo@dawnhealth.com>
2024-11-15 12:56:22 +0100

    Initial Commit

---
+++ TCASharedStateNil/ContentView.swift
@@ -0,0 +1,22 @@
+//
+// Copyright © 2024 Dawn Health.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCASharedStateNil/TCASharedStateNilApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2024 Dawn Health.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct TCASharedStateNilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
